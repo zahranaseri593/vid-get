48b0706d980a6596f162f7a4913b21cdddf0ebfd
Zahra Naseri <zahranaseri593@gmail.com>
2023-08-21 18:07:44 +0330

    nav
    responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+'use client'
+
+import { useState } from 'react'
 import { CustomButton, NavLinks, SearchBox } from '.'
+import { Bars3Icon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const [collapse, setCollapse] = useState(false)
+
   return (
     <header className='w-full bg-neutral-900'>
-        <nav className='navbar'>
-            <p className='navbar__logo'>Vid.Get</p>
-            <SearchBox />
-            <NavLinks />
-            <div className='flex gap-3'>
-                <CustomButton
-                    title="Log in"
-                    containerStyles=""
-                />
-                <CustomButton
-                    title="sign up"
-                    containerStyles="bg-white text-neutral-900"
-                />
+        <nav className='navbar relative'>
+            <p className='navbar__logo lg:mr-14'>Vid.Get</p>
+            {/* navbar collapse */}
+            <div className={`${collapse? 'hidden': 'flex'} navbar__collapse`}>
+                <SearchBox />
+                <NavLinks />
+                <div className='flex gap-3 lg:flex-row'>
+                    <CustomButton
+                        title="Log in"
+                        containerStyles=""
+                    />
+                    <CustomButton
+                        title="sign up"
+                        containerStyles="bg-white text-neutral-900"
+                    />
+                </div>
             </div>
+
+            {/* menu icon */}
+            <Bars3Icon className='h-10 cursor-pointer lg:hidden' onClick={()=> setCollapse(prev => !prev)} />
         </nav>
     </header>
   )
