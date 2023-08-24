bba6dafd04a55590e8208a3f8f1943ddd3c3bbf7
Zahra Naseri <zahranaseri593@gmail.com>
2023-08-24 15:22:33 +0330

    ...
    feed

--- app/page.tsx
+++ app/page.tsx
@@ -1,78 +1,39 @@
+'use client'
+
 import { VideoCard } from '@/components'
-import Image from 'next/image'
-
-const videoData = {
-  "id": 2499611,
-  "width": 1080,
-  "height": 1920,
-  "url": "https://www.pexels.com/video/2499611/",
-  "image": "https://images.pexels.com/videos/2499611/free-video-2499611.jpg?fit=crop&w=1200&h=630&auto=compress&cs=tinysrgb",
-  "full_res": null,
-  "tags": [],
-  "duration": 22,
-  "user": {
-    "id": 680589,
-    "name": "Joey Farina",
-    "url": "https://www.pexels.com/@joey"
-  },
-  "video_files": [
-    {
-      "id": 125004,
-      "quality": "hd",
-      "file_type": "video/mp4",
-      "width": 1080,
-      "height": 1920,
-      "fps": 23.976,
-      "link": "https://player.vimeo.com/external/342571552.hd.mp4?s=6aa6f164de3812abadff3dde86d19f7a074a8a66&profile_id=175&oauth2_token_id=57447761"
-    },
-    {
-      "id": 125005,
-      "quality": "sd",
-      "file_type": "video/mp4",
-      "width": 540,
-      "height": 960,
-      "fps": 23.976,
-      "link": "https://player.vimeo.com/external/342571552.sd.mp4?s=e0df43853c25598dfd0ec4d3f413bce1e002deef&profile_id=165&oauth2_token_id=57447761"
-    },
-    {
-      "id": 125006,
-      "quality": "sd",
-      "file_type": "video/mp4",
-      "width": 240,
-      "height": 426,
-      "fps": 23.976,
-      "link": "https://player.vimeo.com/external/342571552.sd.mp4?s=e0df43853c25598dfd0ec4d3f413bce1e002deef&profile_id=139&oauth2_token_id=57447761"
-    }
-  ],
-  "video_pictures": [
-    {
-      "id": 308178,
-      "picture": "https://static-videos.pexels.com/videos/2499611/pictures/preview-0.jpg",
-      "nr": 0
-    },
-    {
-      "id": 308179,
-      "picture": "https://static-videos.pexels.com/videos/2499611/pictures/preview-1.jpg",
-      "nr": 1
-    },
-    {
-      "id": 308180,
-      "picture": "https://static-videos.pexels.com/videos/2499611/pictures/preview-2.jpg",
-      "nr": 2
-    },
-    {
-      "id": 308181,
-      "picture": "https://static-videos.pexels.com/videos/2499611/pictures/preview-3.jpg",
-      "nr": 3
-    }
-  ]
-}
-
-
-const dataArray = [1,2,3,4,5,6,7,8,9,1]
+import { VideoProps } from '@/types'
+import { useEffect, useState } from 'react'  
 
 export default function Home() {
-  return (
+
+  const [loading, setLoading] = useState(false)
+  const [videos, setVideos] = useState< object | VideoProps>({})
+
+  const fetchVideos = async () => {
+    try {
+      setLoading(true)
+      const response = await fetch('https://api.pexels.com/videos/popular',{
+         method: "GET",
+         headers: {
+           "Authorization": "bFzTf9c6Eg2xn8DpkwlWwWTwgh1KxPqEKVESvzZXLIWFx1BNZBUvg1Yh"
+         },})
+         const data = await response.json()
+         setVideos(data)
+    } catch (error) {
+      console.log(error)
+    }finally{
+      setLoading(false)
+    }
+  };
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
+
+  if(loading) return <p>Loading...</p>
+
+  if(videos.videos) return (
     <>
     <div className="w-full">
 
@@ -83,16 +44,16 @@ export default function Home() {
           <p className='text-xl'>find videos you like, get them with just a click and use them !</p>
         </div>
         <VideoCard
-          video={videoData}
+          video={videos.videos[0]}
           containerStyle="lg:col-span-2 xl:col-span-3 md:col-span-1 row-span-2"
         />
       
-        {dataArray.map((item) => (
-          <VideoCard
-            video={videoData}
-            containerStyle="row-span-3"
-            key={videoData.id}
-        />
+        {videos.videos.slice(1).map((item : VideoProps) => (
+           <VideoCard
+             video={item}
+             containerStyle="row-span-3"
+             key={videos.id}
+         />
         ))}
       </div>
         
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -13,24 +13,26 @@ const VideoCard = ({video,containerStyle}: VideoCardProps) => {
   return (
     <div className={`${containerStyle} min-h-[20rem] md:min-h-[30rem] `}>
        <Link href='/' >
-        <div className='bg-cover bg-no-repeat bg-center w-full h-full rounded-2xl' style={{backgroundImage: `url('${video.image}')`}}>
+        <div className='bg-css w-full h-full rounded-2xl' style={{backgroundImage: `url('${video.image}')`}}>
         <div className='h-full flex flex-col justify-between p-2'>
         <div className=' w-full flex justify-end gap-2'>
           <span className="bg-neutral-50 p-2 rounded-full bg-opacity-75"><HeartIcon 
-            className='h-3 text-neutral-900 '
+            className={`h-3 text-neutral-900`}
           /></span>
           <span className="bg-neutral-50 p-2 rounded-full bg-opacity-75"><PlusIcon 
             className='h-3 text-neutral-900 '
           /></span>
         </div>
         
-        <div className='flex'>
-          <div className='bg-red-200 p-6 rounded-full mr-2'></div>
+        <Link href={video.user.url} className='flex'>
+          <div 
+          className='bg-css p-6 rounded-full mr-2' 
+          style={{backgroundImage: `url(https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI=)`,backgroundSize: '150%'}}/>
           <div>
-            <p>John Doe</p>
-            <p>The Artist</p>
+            <p>{video.user.name}</p>
+            <p>Artist</p>
           </div>
-        </div>
+        </Link>
       
         </div>
         </div>
